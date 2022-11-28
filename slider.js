a28b5dd58424e3b3b4cc0a7c04c09851a8c42aca
batrung2204 <114908362+batrung2204@users.noreply.github.com>
2022-11-28 16:38:59 +0700

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,72 @@
+$('.slider').each(function(){//for ever slider   
+    var $this = $(this);//current slider
+    var $group = $this.find('.slide-group');//get the slide-group(container)
+    var $slides = $this.find('.slide');
+    var buttonArray =[];
+    var currentIndex = 0;
+    var timeout;
+
+    function move(newIndex) {
+        var animateLeft, slideLeft;
+
+        advance();  
+
+        if($group.is(':animated') || currentIndex === newIndex){
+            return;
+        }
+
+        buttonArray[currentIndex].removeClass('active');
+        buttonArray[newIndex].addClass('active');
+
+        if(newIndex > currentIndex){
+            slideLeft = '100%';
+            animateLeft = '-100%';
+        } else {
+            slideLeft = '-100%';
+            animateLeft = '100%';
+        }
+
+        $slides.eq(newIndex).css({
+            left: slideLeft,
+            display: 'block' 
+        });
+
+        $group.animate({
+            left: animateLeft
+        }, function(){
+            $slides.eq(currentIndex).css({
+                display: 'none'
+            });
+            $slides.eq(newIndex).css({
+                left: 0
+            });
+            $group.css({
+                left: 0
+            });
+            currentIndex = newIndex;
+        });
+    }
+    function advance(){
+        clearTimeout(timeout);
+        timeout = setTimeout(function(){
+            if(currentIndex < ($slides.leng - 1)){
+                move(currentIndex + 1);
+            } else{
+                move(3);
+            }
+        }, 1000)
+        console.log("10000");
+        ;
+    }
+    $.each($slides, function(index){
+        var $button =$('<button tyde ="button" class="slide-btn">&bull;</button>');
+        if(index === currentIndex){
+            $button.addClass('active');
+        }
+        $button.on('click', function(){
+            move(index);
+        }).appendTo('.slide-buttons');
+        buttonArray.push($button);
+    });
+    advance();
+});
